9715e77609535b5c03a79b38463869b7aed89c2f
rilian <dmitriyis@gmail.com>
2014-08-15 14:16:19 -0400

    get sprints report

---
+++ 7_get_sprints_report.rb
@@ -0,0 +1,50 @@
+require 'dotenv'
+Dotenv.load
+require 'byebug'
+
+require_relative 'db'
+
+puts 'Produce Sprints plan'
+
+raw = ActiveRecord::Base.connection.execute('
+  SELECT
+    id,
+    story_name,
+    story_estimate,
+    story_resource
+  FROM sprints
+  ORDER BY id ASC
+')
+
+f = File.open('tmp/sprints.html', 'w')
+f.write('
+<html>
+  <head>
+    <title>Sprints</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>Sprint #</th>
+        <th>Story Name</th>
+        <th>Story Estimate</th>
+        <th>Story Resource</th>
+      </tr>')
+
+raw.values.each do |value|
+  f.write("
+      <tr>
+        <td>#{value[0].strip}</td>
+        <td>#{value[1].strip}</td>
+        <td>#{value[2].strip}</td>
+        <td>#{value[3].strip}</td>
+      </tr>")
+end
+
+f.write('
+    </table>
+  </body>
+</html>')
+
+f.close
--- export.rb
+++ export.rb
@@ -10,5 +10,6 @@ require_relative '3_import_stories'
 require_relative '4_set_feature_id_to_story'
 require_relative '5_get_stories_report'
 require_relative '6_import_sprints'
+require_relative '7_get_sprints_report'
 
 puts 'Done'
