8a8dde1bfe8e8f25abe4ef1c558e79e6a43266ce
rilian <dmitriyis@gmail.com>
2014-08-16 14:12:17 -0400

    add feature name to sprints report

--- 7_get_sprints_report.rb
+++ 7_get_sprints_report.rb
@@ -9,6 +9,7 @@ puts 'Produce Days report'
 raw = ActiveRecord::Base.connection.execute('
   SELECT
     id,
+    feature_name,
     story_name,
     story_estimate,
     story_project_id
@@ -27,7 +28,8 @@ f.write('
     <table border="1">
       <tr>
         <th>Day #</th>
-        <th>Story Name</th>
+        <th>Feature</th>
+        <th>Story</th>
         <th>Story Estimate</th>
         <th>Story Project</th>
       </tr>')
@@ -38,7 +40,7 @@ raw.values.each do |value|
   if value[0].to_i % sprint_days == 0 && sprints[value[0].to_i / sprint_days].nil?
     sprints[value[0].to_i / sprint_days] = true
     f.write("
-      <tr><th colspan=\"4\">Sprint #{(value[0].to_i / sprint_days).to_i + 1}</th></tr>")
+      <tr><th colspan=\"5\">Sprint #{(value[0].to_i / sprint_days).to_i + 1}</th></tr>")
   end
 
   f.write("
@@ -46,7 +48,8 @@ raw.values.each do |value|
         <td>#{value[0].to_i + 1}</td>
         <td>#{value[1].strip}</td>
         <td>#{value[2].strip}</td>
-        <td>#{ENV["#{value[3]}_NAME"]}</td>
+        <td>#{value[3].strip}</td>
+        <td>#{ENV["#{value[4]}_NAME"]}</td>
       </tr>")
 end
 
